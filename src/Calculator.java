3331d6a8824a6dd7352e5c855298fc60a4454f29
Andrzej Sędzik <andrzej.sedzik.dev@gmail.com>
2022-08-18 22:19:19 +0200

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+
+    static public int add(int a, int b) {
+        return a + b;
+    }
+    static public int subtract(int a, int b) {
+        return a - b;
+    }
+    public static void main(String[] args) {
+        System.out.println( add(5, 8));
+        System.out.println(subtract(5,2));
+    }
+}
+
